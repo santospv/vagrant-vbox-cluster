e16cc1ffa1ab217201434be73953067cd6568f8f
Paulo Vitor <paulovitor8@gmail.com>
2022-10-22 20:54:41 -0300

    Corrigindo diretório do Wget + Descricao da pagina

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,5 +1,5 @@
 sudo su
-wget https://github.com/santospv/container-apache/archive/refs/heads/main.zip
+wget https://github.com/santospv/vagrant-vbox-cluster/archive/refs/heads/main.zip
 apt update && sudo apt install unzip -y
 unzip main.zip
 rm -rf main.zip
@@ -8,7 +8,7 @@ mkdir app
 cd app/
 mkdir _data
 cd _data/
-cp -r /home/vagrant/container-apache-main/* .  
+cp -r /home/vagrant/vagrant-vbox-cluster-main/site/* .  
 rm -rf /home/vagrant *
 #criando o cluster para rodar em 10 replicas de nodes já com volume fixo da applicação
 docker service create --name meu-app --replicas 10 -dt -p 80:80 --mount type=volume,src=app,dst=/app/ webdevops/php-apache:alpine-php7
